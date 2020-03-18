490f997eddfab451654b3c329732200929645331
LeLe673893702 <673893702@qq.com>
2020-03-18 22:59:03 +0800

    Init Commit

---
+++ app/src/main/java/com/newler/ark_im_socket/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.newler.ark_im_socket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
